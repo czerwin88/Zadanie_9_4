e2c5712e2f20a16564fd2f6c938313e127a48144
Czerwin <r.j.czerwinski@gmail.com>
2017-06-06 21:14:40 +0200

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function drawTree(size) {
-  for (var i = 0; i <= size; i++) {
+  for (var i = 0; i < size; i++) {
     var line = '';
-    for (var j = 0; j < i; j++) {
+    for (var j = 0; j <= i; j++) {
       line += '*';
     }
-    for (var k = 0; k <= 5; k--) {
-      line -= '*';
-    }
-    console.log(star);
+
+    console.log(line);
   }
 }
+
+drawTree(1)
